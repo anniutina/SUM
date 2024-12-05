1dd7709dd4edcf1eee82338df4094aa856403be7
Hanna Vasiutina <hanna@Hannas-MacBook-Pro-4.local>
2024-12-05 08:06:06 +0100

    added simulate_ASC,  updates to functions, sum_main_msa_kpi

--- src/functions.py
+++ src/functions.py
@@ -11,6 +11,7 @@ import requests
 from pyproj import Transformer
 import geopandas as gpd
 import math
+from scipy import optimize
 
 from ExMAS import main
 from query_PT import main as qpt_main
@@ -185,34 +186,114 @@ def run_ExMAS(df, inData, params, hub=None, degree=8):
     
     inData = main.main(inData, params)
 
-#  TODO: update function
-def simulate(gdf_areas, df_demo, gdf_centroid, od, od_probs, hubs, inData, params, OTP_API, degree=1, N=1, ASC=2.58):
-    '''calculate utilities for each traveller of the given area, (single rides, no ExMAS)
+
+def calc_E_p_sum(df, ASC=0):
+    df_p = df.copy()
+    df_p['u_SUM_OD_'] = df_p.u_SUM_OD + ASC
+    df_p['p_SUM_'] = df_p.apply(lambda row: math.exp(-row.u_SUM_OD_) / \
+                        (math.exp(-row.u_SUM_OD_) + math.exp(-row.u_PT_OD)), axis=1)
+    return df_p.p_SUM_.mean()
+
+
+def simulate_MSA(gdf_areas, df_demo, gdf_centroid, od, od_probs, hubs, inData, params, OTP_API, 
+                 degree=1, N=1, max_iter=100, ASC=1.95, results_period=0) -> dict:
+    '''calculate utilities for each traveller of the given area for single and shared rides (ExMAS)
+        calculate ASC if ASC = None
+        apply method of successive averages (MSA) for travel times variable
+
     input:  gdf_areas - SUM areas [geopandas dataframe]
-            df_demo - city demographic [csv]
-            gdf_centroid - centroids of city zones [geojson]
-            od - ODMs [excel]
-            od_probs - ODM with probabilities [excel]
-            N - number of replications
-            ASC - alternative specific constant
-    output: tuple[0] - dictionary with mean results for N iterations for each area sample
-               ex. {'Skotniki': tw_PT_OD, tw_PT_HD, u_PT_OD, u_PT_HD, u_SUM_OD, p_SUM}
-            tuple[1] - dictionary with results of the last iteration for sample of each area:
-               ex. {'Skotniki': origin_x, origin_y, destination_x, destination_y, treq, u_PT_OD,
-                                origin, hub, dist, ttrav, tarr, u, u_SUM_OD, p_SUM}
-            '''
-    areas_res = {}
-    sum_res = {} # resultind dataFrame from the last iteration
+        df_demo - city population distribution [csv]
+        gdf_centroid - centroids of city zones [geojson]
+        od - ODMs [excel], od_probs - dataframe with destination probabilities
+        hubs - choseen hub locations [dict]
+        inData, params, OTP_API, degree - ExMAS input
+        N - number of replications (generate sample for the area)
+        iterations - number of iterations for ExMAS
+        ASC - alternative specific constant
+    output: dictionary {'area name': {'avg_sim_res': DataFrame}, {'sum_res': DataFrame}, {}}
+        avg_sim_res - mean results for N replications for each area sample
+            ex. {'Skotniki': {'avg_sim_res': ['tw_PT_OD', 'tw_PT_HD', 'u_PT_OD', 'u_PT_HD', 'u_SUM_OD', 'p_SUM']}}
+        sum_res - results of the last iteration for sample of each area:
+            ex. {'Skotniki': {'sum_res': [origin_x, origin_y, destination_x, destination_y, treq, u_PT_OD,
+                                origin, hub, dist, ttrav, tarr, u_SUM_OD, p_SUM]}}
+        kpis_res - DataFrame with KPIs [] TODO: add labels
+        avg_times - average expected times [t_expected_i] for all travelers 
+        times - DataFrame with times, ex. {'Skotniki': t_0, ttrav_sh_i, t_expected_i}
+        asc_res - asc coefficients for the case when ASC is unknown
+    '''
+    results = {} # main dictionary to store all the results
+    # keys = ['avg_sim_res', 'sum_res', 'avg_times', 'times', 'kpis_res', 'asc_res']
+    
+    def iterate(df_sum):
+        ''' inner function to perform iterations to evaluate results for options:
+            1. before the system stabilizes -> apply MSA
+            2. afre stabilization -> to obtain KPIs
+        '''
+        sum_demand = df_sum[df_sum.apply(lambda row: random.random() < row['p_SUM'], axis=1)] # chosen SUM travelers
+        # print('sum_demand sample', sum_demand.shape[0])
+        df_sum_sh = sum_demand[['origin_x', 'origin_y', 'destination_x', 'destination_y', 'treq', 'origin', 'destination', 'dist', 't_expected', 'u_PT_OD', 'nTrips']]
+        df_sum_sh['nTrips'] = df_sum_sh['nTrips'] + 1
+        
+        run_ExMAS(df_sum_sh, inData, params, hub, degree)
+        
+        # sort data after ExMAS shuffled trips to assign ttrav_sh to SUM travelers
+        inData_reqs_sorted = inData.sblts.requests.sort_values('index') 
+        inData_reqs_sorted.set_index('index', inplace=True)
+        inData_reqs_sorted = inData_reqs_sorted.rename_axis(None)
+        df_sum_sh['ttrav_sh'] = inData_reqs_sorted['ttrav_sh']
+        df_sum_sh['tarr'] = df_sum_sh.treq + df_sum_sh.apply(
+            lambda req: pd.Timedelta(req.ttrav_sh, 's').floor('s'), axis=1)
+    
+        df_sum_sh['t_expected'] = (df_sum_sh['nTrips'] - 1) / df_sum_sh['nTrips'] * df_sum_sh.t_expected + \
+            df_sum_sh.ttrav_sh / df_sum_sh['nTrips']
+    
+        # assign indexes of shared trips to column 'indexes_sum' and RESET INDEX to calculate OTP
+        df_sum_sh.insert(0, 'indexes_sum', df_sum_sh.index)
+        df_sum_sh = df_sum_sh.reset_index(drop=True)
+
+        # Utility for PT HD
+        u_pt_hd = df_sum_sh[['indexes_sum', 'origin_x', 'origin_y', 'destination_x', 'destination_y', 'tarr']]
+        # change origin x, y to hub coordinates
+        u_pt_hd['origin_x'], u_pt_hd['origin_y'] = hub
+        # treq for PT_HD = treq + ttrav_sh = tarr
+        u_pt_hd['treq'] = pd.to_datetime(u_pt_hd.tarr)
+        u_pt_hd = run_OTP(u_pt_hd, OTP_API)
+        PT_utility(u_pt_hd, params)
+
+        # drop rows with unsuccessful HD trips only for df_sum_sh (as u_PT_OD is the same as for the single trips)
+        df_sum_sh = df_sum_sh.loc[u_pt_hd.index, :] 
+        df_sum_sh.reset_index(drop=True, inplace=True)
+        u_pt_hd.reset_index(drop=True, inplace=True)
+        
+        # Utility F OH - ADD t_w from ExMAS.sblts.rides
+        df_sum_sh['u_F'] = df_sum_sh.apply(lambda request: request['t_expected'] * params.VoT * params.WtS + \
+                                request['dist'] * params.price / 1000, axis=1)
+                                
+        # df_sum['u_PT_OD'] doesn't change for SUM demand
+        df_sum_sh['u_PT_HD'] = u_pt_hd.u_PT
+        df_sum_sh['u_SUM_OD'] = df_sum_sh.u_F + u_pt_hd.u_PT + ASC
+        df_sum_sh['p_SUM'] = df_sum_sh.apply(lambda row: math.exp(-row.u_SUM_OD) / \
+                                (math.exp(-row.u_SUM_OD) + math.exp(-row.u_PT_OD)), axis=1)
+        return df_sum_sh
+
+    
     for _, area in gdf_areas.iterrows():
         key = area["name"]
         dfres = pd.DataFrame()
+        df_kpi_res = pd.DataFrame()
+        ascs = [] # store ASC for each sample of each area
+            
         for repl in range(N):
-            # print("area ", key, " iteration # ", repl + 1)
+            converged = False # TODO: TEST!! if condition
+            df_times = pd.DataFrame()
+            df_kpis = pd.DataFrame()
+            avg_time = []
             area_reqs = define_demand(area, df_demo, gdf_centroid, od, od_probs, params)
             df = area_reqs[key].copy() # df with {O, D, Treq} for the area
             hub = hubs[key]
+            # print(repl + 1, "run simulations for area", key, ": sample size ", df.shape[0])
 
-            # Utility for PT OD
+            # OPTION I: Utility for PT OD
             u_pt_od = df.copy()
             u_pt_od = run_OTP(u_pt_od, OTP_API) # define PT routes for each traveller
             PT_utility(u_pt_od, params)  
@@ -220,30 +301,23 @@ def simulate(gdf_areas, df_demo, gdf_centroid, od, od_probs, hubs, inData, param
             df = df.loc[u_pt_od.index, :] # select requests with successful OD trips 
             df.reset_index(drop=True, inplace=True)
 
-            # Utility for SUM (NSM OH + PT HD)
+            # OPTION II: Utility for SUM (SINGLE TRIPS OH + PT HD)
             df_sum = df.copy()
-            df_sum['u_PT_OD'] = u_pt_od.u_PT
 
-            if degree == 1:
-                df_sum['origin'] = df_sum.apply(lambda row: ox.nearest_nodes(inData.G, row['origin_x'], row['origin_y']), axis=1)
-                df_sum['hub'] = ox.nearest_nodes(inData.G, hub[0], hub[1])
-                df_sum['dist'] = df_sum.apply(lambda request: inData.skim.loc[request.origin, request.hub], axis=1)
-                df_sum['ttrav'] = df_sum['dist'].apply(lambda request: request / params.avg_speed)
-                df_sum['tarr'] = df_sum.treq + df_sum.apply(lambda df_sum: pd.Timedelta(df_sum.ttrav, 's').floor('s'), axis=1)
-                df_sum['u'] = df_sum.apply(lambda request: request['ttrav'] * params.VoT + request['dist'] * params.price / 1000, axis=1)
-            else:
-                run_ExMAS(df_sum, inData, params, hub, degree)
-                df_sum['tarr'] = df_sum.treq + inData.sblts.requests.apply(
-                    lambda request: pd.Timedelta(request.ttrav_sh, 's').floor('s'), axis=1)
-                df_sum['u'] = inData.sblts.requests.u
-                df_sum['u_sh'] = inData.sblts.requests.u_sh
+            # Utility for SINGLE TRIPS OH
+            df_sum['origin'] = df_sum.apply(lambda row: ox.nearest_nodes(inData.G, row['origin_x'], row['origin_y']), axis=1)
+            df_sum['destination'] = ox.nearest_nodes(inData.G, hub[0], hub[1])
+            df_sum['dist'] = df_sum.apply(lambda request: inData.skim.loc[request.origin, request.destination], axis=1)
+            df_sum['ttrav'] = df_sum['dist'].apply(lambda request: request / params.avg_speed)
+            df_sum['tarr'] = df_sum.treq + df_sum.apply(lambda df_sum: pd.Timedelta(df_sum.ttrav, 's').floor('s'), axis=1)
+            df_sum['t_expected'] = df_sum['ttrav'] # optimistic travel time
 
             # Utility for PT HD
-            u_pt_hd = df.copy()
-            u_pt_hd = u_pt_hd.rename(columns = {'treq': 'treq_origin'})
-            u_pt_hd['origin_x'] = hub[0]
-            u_pt_hd['origin_y'] = hub[1]
-            u_pt_hd['treq'] = pd.to_datetime(df_sum.tarr) + pd.Timedelta(params.transfertime, unit='s') # treq for PT_HD
+            u_pt_hd = df_sum[['origin_x', 'origin_y', 'destination_x', 'destination_y', 'tarr']]
+            # change origin x, y to hub coordinates
+            u_pt_hd['origin_x'], u_pt_hd['origin_y'] = hub
+            # treq for PT_HD = treq + ttrav + transfertime
+            u_pt_hd['treq'] = pd.to_datetime(u_pt_hd.tarr) + pd.Timedelta(params.transfertime, unit='s')
             u_pt_hd = run_OTP(u_pt_hd, OTP_API)
             PT_utility(u_pt_hd, params)
 
@@ -252,25 +326,111 @@ def simulate(gdf_areas, df_demo, gdf_centroid, od, od_probs, hubs, inData, param
             df_sum = df_sum.loc[u_pt_hd.index, :] 
             df_sum.reset_index(drop=True, inplace=True)
             u_pt_hd.reset_index(drop=True, inplace=True)
-            df_sum['u_PT_HD'] = u_pt_hd.u_PT
             
-            if degree == 1:
-                df_sum['u_SUM_OD'] = df_sum.u + u_pt_hd.u_PT + ASC
-            else:
-                df_sum['u_SUM_OD'] = df_sum.u_sh + u_pt_hd.u_PT + ASC
+            df_sum['u_PT_OD'] = u_pt_od.u_PT # utility for PT OD
+            df_sum['u_PT_HD'] = u_pt_hd.u_PT # utility for PT HD
+            # Utility for SUM OD
+            df_sum['u_SUM_OD'] = df_sum.apply(lambda request: request['ttrav'] * params.VoT * params.WtS + \
+                                                request['dist'] * params.price / 1000, axis=1) + u_pt_hd.u_PT + ASC if ASC else \
+                                 df_sum.apply(lambda request: request['ttrav'] * params.VoT * params.WtS + \
+                                                request['dist'] * params.price / 1000, axis=1) + u_pt_hd.u_PT
+            # probability of using SUM for each traveler in a single trip
             df_sum['p_SUM'] = df_sum.apply(lambda row: math.exp(-row.u_SUM_OD) / \
-                                           (math.exp(-row.u_SUM_OD) + math.exp(-row.u_PT_OD)), axis=1)
-            df_means = pd.DataFrame([[u_pt_od.waitingTime.mean(), u_pt_hd.waitingTime.mean(), u_pt_od.u_PT.mean(),
-                                    u_pt_hd.u_PT.mean(), df_sum.u_SUM_OD.mean(), df_sum.p_SUM.mean()]], 
-                                    columns=['tw_PT_OD', 'tw_PT_HD', 'u_PT_OD', 'u_PT_HD', 'u_SUM_OD', 'p_SUM'])
-            dfres = pd.concat([dfres, df_means], ignore_index=True)
-        sum_res[key] = df_sum
-        areas_res[key] = dfres
-    return (areas_res, sum_res)
+                                          (math.exp(-row.u_SUM_OD) + math.exp(-row.u_PT_OD)), axis=1)
+            # print("p_SUM ",  df_sum.p_SUM.mean())
+            
+            # ASC calculation
+            if not ASC:
+                degree = 1
+                ascs.append(optimize.fsolve(lambda x: calc_E_p_sum(df_sum, x) - params.expected_prob, 0)[0])
+                # print(repl, "ASC ", key, ascs[repl])
+              
+            # times ---------------------------------------------------
+            # optimistic ttrav, updated after each ExMAS iteration for travelers using SUM
+            df_times['t_0'] = df_sum['ttrav']
+            avg_time.append(df_sum.ttrav.mean()) # optimistic average travel time for ALL travelers before shared travel
+                
+            df_sum['nTrips'] = 0 # number of shared trips
+                
+            if degree > 1:
+                i = 1
+                while i < max_iter + 1 and not converged:
+                # loop will run until the system will stabilize or until max_iter
+                    # print('iteration ', i, ' for', key)
+                    
+                    df_sum_sh = iterate(df_sum)
 
-def calc_E_p_sum(df, ASC=0):
-    df_p = df.copy()
-    df_p['u_SUM_OD_'] = df_p.u_SUM_OD + ASC
-    df_p['p_SUM_'] = df_p.apply(lambda row: math.exp(-row.u_SUM_OD_) / \
-                        (math.exp(-row.u_SUM_OD_) + math.exp(-row.u_PT_OD)), axis=1)
-    return df_p.p_SUM_.mean()
+                    # times ---------------------------------------------------
+                    # dataframe with times: {t_0, t_ttrav_sh_i, t_expected_i}
+                    df_times.loc[df_sum_sh.index, [f'ttrav_sh_{i}']] = df_sum_sh[['ttrav_sh']].values
+                    df_times.loc[df_sum_sh.index, [f't_expected_{i}']] = df_sum_sh[['t_expected']].values
+
+                    # Update p_SUM and t_expected for trips chosen for SUM in the main table
+                    df_sum.loc[df_sum_sh['indexes_sum'], ['p_SUM']] = df_sum_sh[['p_SUM']].values
+                    df_sum.loc[df_sum_sh['indexes_sum'], ['t_expected']] = df_sum_sh[['t_expected']].values
+                    df_sum.loc[df_sum_sh['indexes_sum'], ['nTrips']] = df_sum_sh[['nTrips']].values
+                    avg_time.append(df_sum.t_expected.mean()) # average expected travel times for ALL travelers
+                
+                    # STOP ---------------------------------------------------
+                    delta = abs(avg_time[i] - avg_time[i - 1]) / avg_time[i - 1]
+                    # print("delta ", delta)
+                    # print("==============================================================")
+                    # check if convergence criterion is met
+                    if delta < params.convergence_threshold:
+                        converged = True
+                        print(f"Convergence reached at iteration {i}")
+                        # print("===============================================================")
+
+                    i += 1
+                
+                # after the system stabilization, obtain KPI's for the results_period
+                if converged:
+                    # run ExMAS to obtain KPIs
+                    for i in range(results_period):
+                        # print('iteration after converged ', i + 1, ' for', key)
+                        # print("-----------------------------------------------------")
+                        
+                        df_sum_sh = iterate(df_sum)
+                        df_kpis[i] = inData.sblts.res # obtain kpis after the system stabilized
+
+                        # times ---------------------------------------------------
+                        # dataframe with times: {t_0, t_ttrav_sh_i, t_expected_i}
+                        df_times.loc[df_sum_sh.index, [f'ttrav_sh_{i}']] = df_sum_sh[['ttrav_sh']].values
+                        df_times.loc[df_sum_sh.index, [f't_expected_{i}']] = df_sum_sh[['t_expected']].values
+
+                        # Update p_SUM and t_expected for trips chosen for SUM in the main table
+                        df_sum.loc[df_sum_sh['indexes_sum'], ['p_SUM']] = df_sum_sh[['p_SUM']].values
+                        df_sum.loc[df_sum_sh['indexes_sum'], ['t_expected']] = df_sum_sh[['t_expected']].values
+                        df_sum.loc[df_sum_sh['indexes_sum'], ['nTrips']] = df_sum_sh[['nTrips']].values
+                        avg_time.append(df_sum.t_expected.mean()) # average expected travel times for ALL travelers
+                else:
+                    print(f"no convergence after {max_iter} iterations")
+                    print("-----------------------------------------------------")
+
+            df_means = pd.DataFrame([[u_pt_od.waitingTime.mean(), u_pt_hd.waitingTime.mean(), u_pt_od.u_PT.mean(),
+                                        u_pt_hd.u_PT.mean(), df_sum.u_SUM_OD.mean(), df_sum.p_SUM.mean()]], 
+                                        columns=['tw_PT_OD', 'tw_PT_HD', 'u_PT_OD', 'u_PT_HD', 'u_SUM_OD', 'p_SUM'])
+            dfres = pd.concat([dfres, df_means], ignore_index=True)
+            df_kpi_res = pd.concat([df_kpi_res, df_kpis], axis=1)
+        # print("===============================================================")
+        
+        results[key] = {'avg_sim_res': dfres, 'sum_res': df_sum, 'kpis_res': df_kpi_res, 
+                        'avg_times': avg_time, 'times': df_times, 'asc_res': ascs}
+    return results
+
+def calc_KPIs(df):
+    ''' calculates such KPI indicaors: 
+        del_VehHourTrav - vehicle hours reduction,
+        del_PassUtility - travellers utility gains,
+        occupancy,
+        del_PassHourTrav - passenger hours increase,
+        del_fleet_size - potential fleet size reduction
+    input: dataframe with KPIs for each area
+    output: dataframe with calculated KPI indicators for analysis '''
+    for i, row in df.iterrows():
+        df.loc[i, 'del_VehHourTrav'] = abs(row['VehHourTrav'] - row['VehHourTrav_ns']) / row['VehHourTrav_ns']
+        df.loc[i, 'del_PassUtility'] = abs((row['PassUtility'] - row['PassUtility_ns']) / row['PassUtility_ns'])
+        df.loc[i, 'Occupancy'] = row['PassHourTrav'] / row['VehHourTrav']
+        df.loc[i, 'del_PassHourTrav'] = abs((row['PassHourTrav'] - row['PassHourTrav_ns']) / row['PassHourTrav_ns'])
+        df.loc[i, 'del_fleet_size'] = abs((row['fleet_size_shared'] - row['fleet_size_nonshared']) / row['fleet_size_nonshared'])
+    return df
